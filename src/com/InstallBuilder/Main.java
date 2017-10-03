9e30c7a44a9ac8fdd721c9075a5ee1d93896aedd
JudgeGlass <hdwilcox02@gmail.com>
2017-10-03 16:12:56 -0400

    Updated version num

--- src/com/InstallBuilder/Main.java
+++ src/com/InstallBuilder/Main.java
@@ -8,7 +8,7 @@ import com.InstallBuilder.tools.Logger;
 import com.InstallBuilder.windows.InstallBuilderWindow;
 
 public class Main {
-	public static final String version = "v0.0.2.2";
+	public static final String version = "v0.0.2.3";
 	
 	public static void main(String args[]) {
 		Logger log = new Logger("Latest.log");
