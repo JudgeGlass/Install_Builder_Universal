a2b430de6e3c6949b05bbca6a88e325c4f52c62e
JudgeGlass <hdwilcox02@gmail.com>
2017-10-21 15:15:54 -0400

    Somthing went wrong

--- src/com/JudgeGlass/InstallBuilder/Main.java
+++ src/com/JudgeGlass/InstallBuilder/Main.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.JudgeGlass.InstallBuilder;
 import java.awt.Font;
 import java.io.File;
@@ -14,7 +14,7 @@ import com.JudgeGlass.InstallBuilder.windows.InstallBuilderWindow;
 import com.JudgeGlass.Tools.Download.DownloadFile;
 
 public class Main {
-	public static final String versionUrl = "https://github.com/JudgeGlass/Install_Builder_Universal/releases/download/v0.0.3/InstallBuilder_v0.0.4.zip";
+	public static final String versionUrl = "https://github.com/JudgeGlass/Install_Builder_Universal/releases/download/v0.0.5/InstallBuilder_v0.0.5.zip";
 	public static final String version = "v0.0.5";
 	public static final String buildDate = "10/19/17";
 	public static final boolean isDebug = false;
@@ -70,55 +70,4 @@ public class Main {
 		} 
 	}
 }
-=======
-package com.JudgeGlass.InstallBuilder;
-import java.awt.Font;
 
-import javax.swing.JOptionPane;
-import javax.swing.UIManager;
-
-import com.JudgeGlass.InstallBuilder.tools.Logger;
-import com.JudgeGlass.InstallBuilder.windows.InstallBuilderWindow;
-
-public class Main {
-	public static final String version = "v0.0.4";
-	public static final boolean isDebug = false;
-	
-	public static void main(String args[]) {
-		Logger log = new Logger("latest.log");
-		
-		log.Info("System.getProperty(\"os.name\") == " + System.getProperty("os.name"));
-		log.Info("System.getProperty(\"os.arch\") == " + System.getProperty("os.arch"));
-		log.Info("System.getProperty(\"java.home\") == " + System.getProperty("java.home"));
-		log.Info("System.getProperty(\"java.version\") == " + System.getProperty("java.version"));
-		
-		try {
-			log.Info("Setting UI...");
-			if(!System.getProperty("os.name").equals("Linux")) {
-				JOptionPane.showMessageDialog(null, "OS not supported!\nIf your on windows please get the windows version.\n"
-						+ "https://github.com/JudgeGlass/Install_Builder_Universal/releases/tag/v0.0.4", "OS not supported!", JOptionPane.ERROR_MESSAGE);
-				return;
-			}else if(System.getProperty("os.name").equals("Linux")){
-				UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
-				
-				UIManager.put("Button.font", new Font(Font.MONOSPACED, 0, 10));
-				UIManager.put("Label.font", new Font(Font.MONOSPACED, 0, 4));
-				UIManager.put("RadioButton.font", new Font(Font.MONOSPACED, 0, 10));
-			}
-			
-			log.Info("Starting Program...");
-			InstallBuilderWindow window = new InstallBuilderWindow("Install Builder Universal [" + version + "]", log);
-			window.setVisable(true);
-			
-			if(Main.isDebug) {
-				JOptionPane.showMessageDialog(null, "This is a Install builder " + Main.version + " DEBUG.\nPlease accecpt bugs.", "Debug Warning", JOptionPane.WARNING_MESSAGE);
-			}
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Error:\n" + e);
-			e.printStackTrace();
-			log.Error("Could not load UI: " + e.getMessage());
-			return;
-		} 
-	}
-}
->>>>>>> 703733152c91c6345f2e175520fd6e35388ae591
--- src/com/JudgeGlass/InstallBuilder/listeners/MenuActionListener.java
+++ src/com/JudgeGlass/InstallBuilder/listeners/MenuActionListener.java
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 package com.JudgeGlass.InstallBuilder.listeners;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 
-import com.JudgeGlass.InstallBuilder.tools.Logger;
-import com.JudgeGlass.InstallBuilder.updater.CheckUpdate;
 import com.JudgeGlass.InstallBuilder.windows.AboutWindow;
-import com.JudgeGlass.InstallBuilder.windows.InstallBuilderWindow;
 import com.JudgeGlass.InstallBuilder.windows.LogWindow;
 
 public class MenuActionListener implements ActionListener {
@@ -25,26 +20,3 @@ public class MenuActionListener implements ActionListener {
 		}
 	}
 }
-=======
-package com.JudgeGlass.InstallBuilder.listeners;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import com.JudgeGlass.InstallBuilder.windows.AboutWindow;
-import com.JudgeGlass.InstallBuilder.windows.LogWindow;
-
-public class MenuActionListener implements ActionListener{
-	public void actionPerformed(ActionEvent e) {
-		String cmd = e.getActionCommand();
-		if(cmd.equals("EXIT")) {
-			System.exit(0);
-		}else if(cmd.equals("ABOUT")) {
-			new AboutWindow();
-		}else if(cmd.equals("LOG")) {
-			LogWindow logWindow = new LogWindow();
-			logWindow.setVisible(true);
-		}
-	}
-}
->>>>>>> 703733152c91c6345f2e175520fd6e35388ae591
